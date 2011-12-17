c23fa495b4f2dd9ace48a9082ff353da4141e36a
PTrefall@gmail.com <PTrefall@gmail.com@92bc644c-bee7-7203-82ab-e16328aa9dbe>
2011-12-17 14:34:26 +0000

    small saldo change

--- trunk/src/GeneticAlg/Genome/ProsumerGenome.cpp
+++ trunk/src/GeneticAlg/Genome/ProsumerGenome.cpp
@@ -1,8 +1,8 @@
 #include "ProsumerGenome.h"
 #include <math.h>
 
-ProsumerGenome::ProsumerGenome(double ec, double ep, double ef, double flex, unsigned int policy)
-	: chromosome(ec,ep,ef,flex,policy)
+ProsumerGenome::ProsumerGenome(double ec, double ep, double ef, double flex, unsigned int policy, double saldo)
+	: chromosome(ec,ep,ef,flex,policy,saldo)
 {
 }
 
@@ -13,14 +13,14 @@ ProsumerGenome::~ProsumerGenome()
 double ProsumerGenome::fitness()
 {
 	double energy_consumption_per_hour = chromosome.energy_consumption / chromosome.avg_per_hour_cost_factor;
-	double low_consumption = chromosome.flexi_rate * energy_consumption_per_hour;
+	double low_consumption = chromosome.flexi_rate * chromosome.energy_consumption;
 	double saldo_reduction_factor = (low_consumption / energy_consumption_per_hour)*(low_consumption / energy_consumption_per_hour);
 
 	if(saldo_reduction_factor > 1.0f)
 		saldo_reduction_factor = 1.0f;
 
-	saldo = (1.0 - saldo_reduction_factor) * saldo;
-	return saldo;
+	chromosome.saldo = (1.0 - saldo_reduction_factor) * chromosome.saldo;
+	return chromosome.saldo;
 }
 
 const Prosumer &ProsumerGenome::chromosomeValue() const
