a31c7878ac5a4ee614c597df06faef42044ccc0c
PTrefall@gmail.com <PTrefall@gmail.com@92bc644c-bee7-7203-82ab-e16328aa9dbe>
2011-12-18 11:19:35 +0000

    Added a GAManager that will sit on top of all ga related stuff.

---
+++ trunk/src/GeneticAlg/GAManager.cpp
@@ -0,0 +1,57 @@
+#include "GAManager.h"
+#include "ProsumerGeneticAlg.h"
+//#include "FixedSupplierGeneticAlg.h"
+
+GAManager::GAManager()
+	: prosumerGA(0x0)//, fixedSupplierGA(0x0)
+{
+	prosumerGA = new ProsumerGeneticAlg(
+							100,		//Population Size 
+							0.0,		//Fitness threshold
+							0.05,		//Chance for crossover
+							1,			//Max children from crossover
+							0.05,		//Chance for mutation
+							0.1,		//Start saldo
+							3.0,		//Øk
+							1.0,		//Ep
+							24000.0,	//Ef
+							0.1,		//Flex
+							0);			//Policy
+
+	//fixedSupplierGA = new FixedSupplierGeneticAlg();
+}
+
+GAManager::~GAManager()
+{
+	if(prosumerGA) delete prosumerGA;
+	//if(fixedSupplierGA) delete fixedSupplierGA;
+}
+
+void GAManager::initialize()
+{
+	if(prosumerGA) prosumerGA->initialize();
+	//if(fixedSupplierGA) fixedSupplierGA->initialize();
+}
+
+////////////////////////////////////////////////////
+// PROSUMER HELPERS
+////////////////////////////////////////////////////
+unsigned int GAManager::getPopulationSize() const
+{
+	if(prosumerGA) return prosumerGA->generation->population->individuals.size();
+	else return 0;
+}
+
+////////////////////////////////////////////////////
+// FIXED SUPPLIER HELPERS
+////////////////////////////////////////////////////
+double GAManager::getPriceOffer() const
+{
+	return 0.0;
+}
+
+
+unsigned int GAManager::getCustomerCount() const
+{
+	return 0;
+}
---
+++ trunk/src/GeneticAlg/GAManager.h
@@ -0,0 +1,33 @@
+#pragma once
+
+//#include "ProsumerGeneticAlg.h"
+//#include "FixedSupplierGeneticAlg.h"
+
+class ProsumerGeneticAlg;
+//class FixedSupplierGeneticAlg;
+
+class GAManager
+{
+public:
+	GAManager();
+	~GAManager();
+	void initialize();
+
+//GA getters
+public:
+	ProsumerGeneticAlg *getProsumer() { return prosumerGA; }
+	//FixedSupplierGeneticAlg *getFixedSupplier() { return fixedSupplierGA; }
+
+//PIMPL Helper functions for Prosument GA
+public:
+	unsigned int getPopulationSize() const;
+
+//PIMPL Helper functions for Fixed Supplier GA
+public:
+	double getPriceOffer() const;
+	unsigned int getCustomerCount() const;
+
+private:
+	ProsumerGeneticAlg *prosumerGA;
+	//FixedSupplierGeneticAlg *fixedSupplierGA;
+};
