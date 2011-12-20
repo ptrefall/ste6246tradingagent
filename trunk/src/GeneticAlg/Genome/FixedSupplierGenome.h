6fde6216b063c2b4b51518fed4684bf6cdc13158
PTrefall@gmail.com <PTrefall@gmail.com@92bc644c-bee7-7203-82ab-e16328aa9dbe>
2011-12-20 02:42:48 +0000

    Added participation cost accumulation to the supplier genome

--- trunk/src/GeneticAlg/Genome/FixedSupplierGenome.h
+++ trunk/src/GeneticAlg/Genome/FixedSupplierGenome.h
@@ -17,13 +17,34 @@ public:
 	unsigned int customer_count_ref;
 
 	double participation_cost; //How much do a supplier have to pay in order to participate
+	double participation_cost_accumulator;
 	const double avg_per_hour_factor;
 
 	double reserved_energy;
 
-	FixedSupplier() : price_offer(0.0), actual_price_offer(price_offer), supply_capacity(0.0), saldo(0.0), customer_count(0), customer_count_ref(0), participation_cost(0.0), avg_per_hour_factor(24.0*365.0), reserved_energy(0.0) {}
+	FixedSupplier() 
+		:	price_offer(0.0), 
+			actual_price_offer(price_offer), 
+			supply_capacity(0.0), 
+			saldo(0.0), 
+			customer_count(0), 
+			customer_count_ref(0), 
+			participation_cost(0.0), 
+			avg_per_hour_factor(24.0*365.0), 
+			reserved_energy(0.0), 
+			participation_cost_accumulator(0.0) {}
+
 	FixedSupplier(double po, double sc, double saldo, double pc) 
-		: price_offer(po), actual_price_offer(price_offer), supply_capacity(sc), saldo(saldo), customer_count(0), customer_count_ref(0),  participation_cost(pc), avg_per_hour_factor(24.0*365.0), reserved_energy(0.0) {}
+		:	price_offer(po), 
+			actual_price_offer(price_offer), 
+			supply_capacity(sc), 
+			saldo(saldo), 
+			customer_count(0), 
+			customer_count_ref(0),  
+			participation_cost(pc), 
+			avg_per_hour_factor(24.0*365.0), 
+			reserved_energy(0.0), 
+			participation_cost_accumulator(0.0) {}
 
 	static std::ostream &write(std::ostream& s, FixedSupplier& d)
 	{
